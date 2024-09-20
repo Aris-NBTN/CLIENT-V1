21ad16c50825b49a57584d7028a0fccf881935bf
Aris-NBTN <116520283+Aris-NBTN@users.noreply.github.com>
2024-09-20 13:23:04 +0700

    ND

---
+++ assets/Test-CRFb9HfQ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BLzEd4n4.js";const s=()=>t.jsx(t.Fragment,{});export{s as default};
---
+++ assets/context-HDE3hCe4.js
@@ -0,0 +1,1 @@
+import{r as t,x as c}from"./index-BLzEd4n4.js";function s(){}const u=t.createContext({add:s,remove:s});function m(n){const r=t.useContext(u),o=t.useRef();return c(e=>{if(e){const a=n?e.querySelector(n):e;r.add(a),o.current=a}else r.remove(o.current)})}export{m as u};
