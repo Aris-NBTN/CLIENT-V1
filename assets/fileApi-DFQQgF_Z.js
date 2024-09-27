19f8cad16f662a6c113d7b59f89b91499ed59dfb
Aris-NBTN <116520283+Aris-NBTN@users.noreply.github.com>
2024-09-27 18:35:00 +0700

    ND

---
+++ assets/fileApi-DFQQgF_Z.js
@@ -0,0 +1,1 @@
+import{i as e}from"./indexFile-CjJi2Tza.js";const n="/v1/file",t=async s=>await e.post(`${n}`,s),a=async s=>await e.post(`${n}/base64`,s),r=async s=>await e.post(`${n}/3d`,s),p=async s=>await e.post(`${n}/video`,s),i={uploadFile:t,fileBase64:a,file3D:r,video:p};export{i as f};
