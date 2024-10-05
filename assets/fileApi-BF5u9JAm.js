272f0f104a92f2d92826e94fba21627da1565f4d
Aris-NBTN <116520283+Aris-NBTN@users.noreply.github.com>
2024-10-05 09:11:53 +0700

    ND

--- assets/fileApi-BF5u9JAm.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./indexFile-KYTZc652.js";const n="/v1/file",t=async s=>await e.post(`${n}`,s),a=async s=>await e.post(`${n}/base64`,s),r=async s=>await e.post(`${n}/3d`,s),p=async s=>await e.post(`${n}/video`,s),i={uploadFile:t,fileBase64:a,file3D:r,video:p};export{i as f};
